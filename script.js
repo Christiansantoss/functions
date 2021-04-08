7c986751e9283fa7e8bf1719d2784daa8f705b6a
Christiansantoss <nismoskyi@yahoo.com>
2021-04-08 11:17:48 -0700

    closer look at functions

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+const booking = [];
+
+const createBooking = function (flightnum, numPassengers, price) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  booking.push(booking);
+};
+
+createBooking('LH123');
